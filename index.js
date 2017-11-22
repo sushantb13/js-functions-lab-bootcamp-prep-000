36d2a5bd5e6552ba7e9eca00847e552dafa22800
Sushant Parajuli <sushantb13@gmail.com>
2017-11-22 04:30:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function happyHolidays(string) {
 function happyHolidaysTo(name){
   return "Happy holidays, you!"
 }
+
+function happyHolidayto(holiday, name){
+  return "Happy ${holiday}, ${name}"
+}
